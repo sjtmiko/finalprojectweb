83fb6734a50d180e242a82b46114134cd8c76b38
novanda <dandavanda@gmail.com>
2020-01-07 06:55:00 +0700

    edit in ikrs

--- application/views/informasi/in_krs.php
+++ application/views/informasi/in_krs.php
@@ -1,6 +1,60 @@
 <?php 
 require_once('head.php');?>
 
+<div class="container">
+
+<p>
+Prosedur dan Syarat Cetak Krs Online
+PROSEDUR dan SYARAT CETAK KRS ONLINE
+
+oleh mahasiswa sendiri
+
+ 
+
+Mahasiswa telah menyelesaikan seluruh beban administrasi baik SPP tetap maupun SPP VARIABLE.
+ 
+
+Mahasiswa telah mengisikan KRS Online di amikom.ac.id
+ 
+
+Mahasiswa mencetak sendiri KRS UJIAN melalui Account masing-masing di amikom.ac.id
+ 
+
+Mahasiswa menyimpan KRS UJIAN yang telah dicetak sebagai syarat untuk mengikuti Ujian.
+ 
+
+Pencetakan KRS UJIAN Akan dibuka secara system sesuai tanggal yang ditentukan kemudian
+ 
+
+Mahasiswa yang tidak bisa mencetak sendiri KRS UJIAN nya secara online di amikom.ac.id dipersilahkan melakukan konfirmasi dan verifikasi data di DPK (Direktorat Perencanaan dan Keuangan)
+ 
+
+Mahasiswa yang belum menyelesaikan beban administrasi, maka tidak bisa melakukan pencetakan sendiri secara online dan dimohon segera konfirmasi di DPK dengan membawa bukti kwitansi pembayaran spp variable semester yang bersangkutan.
+ 
+
+ 
+
+KRS UJIAN digunakan untuk syarat ujian, apabila KRS UJIAN hilang maka tidak ada penggantian KRS UJIAN sehingga mahasiswa dipersilahkan mencetak kembali KRS UJIAN melalui online system di amikom.ac.id
+ 
+
+Mahasiswa diharuskan menyimpan dengan baik berkas KRS UJIAN online yang digunakan untuk ujian tengah dan akhir semester, sebagai bahan kroscek data apabila dikemudian hari bermasalah dengan permasalahan Akademis (baik UTS maupun UAS).
+ 
+
+Ukuran Kertas untuk KRS ujian adalah HVS A4 80 gram.
+ 
+
+ 
+
+ 
+
+Yogyakarta, 1 Oktober 2017
+
+DAAK
+
+
+</p>
+
+</div>
 
 
 <?php 
